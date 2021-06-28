2515e5d7d85b6d12e03f1fef1c4557c21043b27e
Hanafi_dev <hanafiahtkj@gmail.com>
2021-06-28 10:36:20 +0700

    #

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -8,7 +8,7 @@
   <!-- General CSS Files -->
   <link rel="stylesheet" href="{{ asset('plugins/bootstrap-4.6.0/css/bootstrap.min.css') }}">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
-  <link rel="manifest" href="{{ asset('manifest.json') }}">
+  <!-- <link rel="manifest" href="{{ asset('manifest.json') }}"> -->
   
   <!-- Extra CSS -->
   @isset($extra_css)
