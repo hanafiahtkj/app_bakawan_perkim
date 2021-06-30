736ebb88b0cc8f787145495cfad044544f7c21bf
Hanafi_dev <hanafiahtkj@gmail.com>
2021-06-30 19:46:12 +0700

    #

--- resources/views/admin/components/footer.blade.php
+++ resources/views/admin/components/footer.blade.php
@@ -4,7 +4,7 @@
         <!-- <div class="bullet"></div>  -->
         <!-- Design By <a href="https://nauval.in/">Muhamad Nauval Azhar</a> -->
     </div>
-    <div class="footer-right">
+    <!-- <div class="footer-right">
         1.0.0
-    </div>
+    </div> -->
 </footer>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -129,9 +129,9 @@
           <!-- <div class="bullet"></div>  -->
           <!-- Design By <a href="https://nauval.in/">Muhamad Nauval Azhar</a> -->
         </div>
-        <div class="footer-right">
+        <!-- <div class="footer-right">
           1.0.0
-        </div>
+        </div> -->
       </footer>
 
     </div>
