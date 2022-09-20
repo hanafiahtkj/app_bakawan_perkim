05c4378df95861bb59d81425aaec445331e2d50a
Hanafi <hanafiahtkj@gmail.com>
2022-09-20 10:06:09 +0800

    #

--- app/Exports/RtlhExport.php
+++ app/Exports/RtlhExport.php
@@ -148,7 +148,7 @@ class RtlhExport implements FromView, WithEvents, WithColumnWidths, ShouldAutoSi
 
         // dd($rtlh);
 
-        //echo view('excel.rtlh', compact('rtlh')); die();
+        echo view('excel.rtlh', compact('rtlh')); die();
 
         return view('excel.rtlh', compact('rtlh'));
     }
@@ -157,38 +157,36 @@ class RtlhExport implements FromView, WithEvents, WithColumnWidths, ShouldAutoSi
     {
         return [
             AfterSheet::class    => function(AfterSheet $event) {
-                // $cellRange = 'A1:AW1';
-                // $event->sheet->getDelegate()->getStyle($cellRange)->getFont()
-                //     ->setSize(12)
-                //     ->setBold(true);
+                $cellRange = 'A1:AW1';
+                $event->sheet->getDelegate()->getStyle($cellRange)->getFont()
+                    ->setSize(12)
+                    ->setBold(true);
 
-                // $event->sheet->getDelegate()->getStyle($cellRange)->getFill()
-                //     ->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID)
-                //     ->getStartColor()->setARGB('E0E0E0');
+                $event->sheet->getDelegate()->getStyle($cellRange)->getFill()
+                    ->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID)
+                    ->getStartColor()->setARGB('E0E0E0');
 
-                // $event->sheet->getDelegate()->getRowDimension('1')->setRowHeight(50);
+                $event->sheet->getDelegate()->getRowDimension('1')->setRowHeight(50);
 
-                // $event->sheet->getDelegate()->getStyle($cellRange)
-                //     ->getAlignment()->setVertical(\PhpOffice\PhpSpreadsheet\Style\Alignment::VERTICAL_CENTER)
-                //     ->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER);
+                $event->sheet->getDelegate()->getStyle($cellRange)
+                    ->getAlignment()->setVertical(\PhpOffice\PhpSpreadsheet\Style\Alignment::VERTICAL_CENTER)
+                    ->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER);
 
-                // $event->sheet->getStyle('A1:AW'.$this->rowCount)->applyFromArray([
-                //     'borders' => [
-                //         'allBorders' => [
-                //             'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN,
-                //             'color' => ['argb' => '000000'],
-                //         ],
-                //     ],
-                // ]);
+                $event->sheet->getStyle('A1:AW'.$this->rowCount)->applyFromArray([
+                    'borders' => [
+                        'allBorders' => [
+                            'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN,
+                            'color' => ['argb' => '000000'],
+                        ],
+                    ],
+                ]);
             },
         ];
     }
 
     public function columnWidths(): array
     {
-        return [
-            'F' => 50,        
-        ];
+        return [];
     }
 
     public function columnFormats(): array
