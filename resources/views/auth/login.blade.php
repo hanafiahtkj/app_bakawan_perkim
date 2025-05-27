546d02d756d9f7338777d9d911be6883d1186a05
Hanafi_dev <hanafiahtkj@gmail.com>
2025-05-27 11:21:25 +0800

    #

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,42 +3,50 @@
     <x-slot name="title">Login</x-slot>
 
     <section class="section">
-      <div class="container mt-5">
-        <div class="row">
-          <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
-            <div class="login-brand">
-            <a href="{{ url('') }}"><img src="{{ asset('img/bakawan-logo.png') }}" alt="logo" width="100"></a>
-            </div>
-
-            <div class="card card-primary">
-              <div class="card-header"><h4>Login</h4></div>
-
-              <div class="card-body">
-                @if ($errors->any())
-                    <div class="alert alert-danger alert-dismissible show fade">
-                      <div class="alert-body">
-                        <button class="close" data-dismiss="alert">
-                          <span>&times;</span>
-                        </button>
-                        <!-- <div class="alert-title">Whoops! Login gagal.</div> -->
-                        Login gagal....
-                            <!-- @foreach ($errors->all() as $error)
-                                <span>{{ $error }}</span>
-                            @endforeach -->
-                      </div>
+        <div class="container mt-5">
+            <div class="row">
+                <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
+                    <div class="login-brand">
+                        <a href="{{ url('') }}"><img src="{{ asset('img/bakawan-logo.png') }}" alt="logo"
+                                width="100"></a>
                     </div>
-                @endif
-                <form method="POST" action="{{ route('login') }}" novalidate="">
-                  @csrf
-                  <div class="form-group">
-                    <label for="email">Username</label>
-                    <input id="username" type="text" class="form-control" name="username" tabindex="1" required autofocus>
-                    <div class="invalid-feedback">
-                      Email tidak boleh kosong
-                    </div>
-                  </div>
 
-                  <!-- <div class="form-group">
+                    <div class="card card-primary">
+                        <div class="card-header">
+                            <h4>Login</h4>
+                        </div>
+
+                        <div class="card-body">
+                            @if ($errors->any())
+                                <div class="alert alert-danger alert-dismissible show fade">
+                                    <div class="alert-body">
+                                        <button class="close" data-dismiss="alert">
+                                            <span>&times;</span>
+                                        </button>
+                                        <!-- <div class="alert-title">Whoops! Login gagal.</div> -->
+                                        Login gagal....
+                                        <!-- @foreach ($errors->all() as $error)
+<span>{{ $error }}</span>
+@endforeach -->
+                                    </div>
+                                </div>
+                            @endif
+                            <form method="POST" action="{{ route('login') }}" novalidate="">
+                                @csrf
+                                <div class="form-group">
+                                    <label for="username">Username</label>
+                                    <input id="username" type="text"
+                                        class="form-control @error('username') is-invalid @enderror" name="username"
+                                        value="{{ old('username') }}" tabindex="1" required autofocus>
+
+                                    @error('username')
+                                        <div class="invalid-feedback">
+                                            {{ $message }}
+                                        </div>
+                                    @enderror
+                                </div>
+
+                                <!-- <div class="form-group">
                     <label for="email">Email</label>
                     <input id="email" type="email" class="form-control" name="email" tabindex="1" required autofocus>
                     <div class="invalid-feedback">
@@ -46,35 +54,37 @@
                     </div>
                   </div> -->
 
-                  <div class="form-group">
-                    <div class="d-block">
-                    	<label for="password" class="control-label">Password</label>
-                      <!-- <div class="float-right">
+                                <div class="form-group">
+                                    <div class="d-block">
+                                        <label for="password" class="control-label">Password</label>
+                                        <!-- <div class="float-right">
                         <a href="auth-forgot-password.html" class="text-small">
                           Forgot Password?
                         </a>
                       </div> -->
-                    </div>
-                    <input id="password" type="password" class="form-control" name="password" tabindex="2" required>
-                    <div class="invalid-feedback">
-                      Password tidk boleh kosong
-                    </div>
-                  </div> 
+                                    </div>
+                                    <input id="password" type="password" class="form-control" name="password"
+                                        tabindex="2" required>
+                                    <div class="invalid-feedback">
+                                        Password tidk boleh kosong
+                                    </div>
+                                </div>
 
-                  <div class="form-group">
-                    <div class="custom-control custom-checkbox">
-                      <input type="checkbox" name="remember" class="custom-control-input" tabindex="3" id="remember-me">
-                      <label class="custom-control-label" for="remember-me">Remember Me</label>
-                    </div>
-                  </div>
+                                <div class="form-group">
+                                    <div class="custom-control custom-checkbox">
+                                        <input type="checkbox" name="remember" class="custom-control-input"
+                                            tabindex="3" id="remember-me">
+                                        <label class="custom-control-label" for="remember-me">Remember Me</label>
+                                    </div>
+                                </div>
 
-                  <div class="form-group">
-                    <button type="submit" class="btn btn-primary btn-lg btn-block" tabindex="4">
-                      Login
-                    </button>
-                  </div>
-                </form>
-                <!-- <div class="text-center mt-4 mb-3">
+                                <div class="form-group">
+                                    <button type="submit" class="btn btn-primary btn-lg btn-block" tabindex="4">
+                                        Login
+                                    </button>
+                                </div>
+                            </form>
+                            <!-- <div class="text-center mt-4 mb-3">
                   <div class="text-job text-muted">Login With Social</div>
                 </div>
                 <div class="row sm-gutters">
@@ -90,16 +100,16 @@
                   </div>
                 </div> -->
 
-              </div>
-            </div>
-            <!-- <div class="mt-5 text-muted text-center">
+                        </div>
+                    </div>
+                    <!-- <div class="mt-5 text-muted text-center">
               Don't have an account? <a href="{{ route('register') }}">Create One</a>
             </div> -->
-            <div class="simple-footer text-white">
-            Copyright &copy; Dinas Perumahan dan Kawasan Permukiman Kota Banjarmasin 2021
+                    <div class="simple-footer text-white">
+                        Copyright &copy; Dinas Perumahan dan Kawasan Permukiman Kota Banjarmasin 2021
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
     </section>
 </x-guest-layout>
